5b963023b1d1102c519a3d36d3ee069648007566
VitorCioletti <ciolettiv@gmail.com>
2017-07-17 21:33:15 -0300

    Starting to make auto update funcionality

---
+++ ConnectionShell/Update/ImagesUri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace riotAPI_teste.Update
+{
+    class ImagesUri
+    {
+       public static string mainImage = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/";
+       public static string profileIcon = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/";
+       public static string spells = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/";
+       public static string passive = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/passive/";
+    }
+}
