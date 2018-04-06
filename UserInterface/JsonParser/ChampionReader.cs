623a119436fe88ad7c918e280328f8385495faf3
Vitor Cioletti <ciolettiv@gmail.com>
2018-04-06 18:01:44 -0300

    Changed references due to compile problem in champions list

--- UserInterface/JsonParser/ChampionReader.cs
+++ UserInterface/JsonParser/ChampionReader.cs
@@ -17,11 +17,11 @@
 				c.Value.image.iconPath = $@"Images\Champions\Square\{c.Value.name}Square.png";
 				c.Value.image.originalLoadingPath = $@"Images\Champions\MainArt\{c.Value.name}_OriginalLoading.jpg";
 
-				c.Value.image.passivePath = $@"Images\Champions\Skills\{c.Value.name}_P.png";
-				c.Value.image.QPath = $@"Images\Champions\Skills\{c.Value.name}_P.png";
-				c.Value.image.WPath = $@"Images\Champions\Skills\{c.Value.name}_W.png";
-				c.Value.image.EPath = $@"Images\Champions\Skills\{c.Value.name}_E.png";
-				c.Value.image.RPath = $@"Images\Champions\Skills\{c.Value.name}_R.png";
+				c.Value.image.passivePath = $@"\UserInterface\Images\Champions\Skills{c.Value.name}_P.png";
+				c.Value.image.QPath = $@"\UserInterface\Images\Champions\Skills{c.Value.name}_P.png";
+				c.Value.image.WPath = $@"\UserInterface\Images\Champions\Skills{c.Value.name}_W.png";
+				c.Value.image.EPath = $@"\UserInterface\Images\Champions\Skills{c.Value.name}_E.png";
+				c.Value.image.RPath = $@"\UserInterface\Images\Champions\Skills{c.Value.name}_R.png";
 
 				return true;
 			}).Select(c => c.Value).OrderBy(c => c.name).ToList();
